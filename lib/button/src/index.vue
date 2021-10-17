4eeb25b782d2885b5acce1c738595bbe383e9020
wdpm <1137299673@qq.com>
2021-10-17 23:04:27 +0800

    save work

--- lib/button/src/index.vue
+++ lib/button/src/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <button
-    :class="[
-      'mad-button',
-    ]"
-  >
-    <span v-if="$slots.default"><slot></slot></span>
+  <button :class="['mad-button', className]" :disabled="isDisabled">
+    <span v-if="loading" class="yx-loading"></span>
+    <span class="yx-btn-content" :style="style">
+      <i v-if="icon" :class="icon"></i>
+      <span v-if="$slots.default"><slot></slot></span>
+    </span>
   </button>
 </template>
 
@@ -15,34 +15,52 @@ export default {
 </script>
 
 <script setup>
-import { defineProps, computed, toRefs } from "vue";
+import { computed, defineProps, toRefs } from "vue";
 
-// const props = defineProps({
-//   type: {
-//     type: String,
-//     default: "default",
-//     validator: (value) =>
-//       ["white", "light", "dark", "black", "text", "primary", "info", "success", "warning", "danger"].includes(value)
-//   },
-//   size: {
-//     type: String,
-//     default: "normal",
-//     validator: (value) => ["small", "normal", "medium", "large"].includes(value)
-//   },
-//   icon: String,
-//   plain: Boolean,
-//   round: Boolean,
-//   circle: Boolean,
-//   block: Boolean,
-//   disabled: Boolean,
-//   loading: Boolean
-// });
+const props = defineProps({
+  type: {
+    type: String,
+    default: "default",
+    validator: (value) => ["success", "primary", "warning", "info", "danger", "default", "text"].includes(value)
+  },
+  size: {
+    type: String,
+    default: "md",
+    validator: (value) => ["lg", "sm", "md"].includes(value)
+  },
+  icon: String,
+  plain: Boolean,
+  round: Boolean,
+  circle: Boolean,
+  block: Boolean,
+  disabled: Boolean,
+  loading: Boolean
+});
 
 const useClass = ({ props, loading }) => {
   return computed(() => {
-    return [];
+    return [
+      props.type ? `yx-btn-${props.type}` : "",
+      props.size ? `yx-btn-${props.size}` : "",
+      {
+        "is-plain": props.plain,
+        "is-round": props.round,
+        "is-circle": props.circle,
+        "is-block": props.block,
+        disabled: props.disabled
+      },
+      loading.value ? "yx-btn-loading" : ""
+    ];
   });
 };
+
+const isDisabled = computed(() => props.loading || props.disabled);
+const { loading, icon } = toRefs(props);
+const className = useClass({ props, loading, icon });
+
+const style = computed(() => {
+  return props.loading ? { opacity: "0", transform: "scale(2.2)" } : {};
+});
 </script>
 
 <style lang="scss">
